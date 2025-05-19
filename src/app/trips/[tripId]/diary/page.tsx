f4391547ce94bb4ef563c79e389f31c10a8cc794
Relish Approvals <compliance@foodstream.in>
2025-05-19 14:39:38 +0530

    Fix route parameter inconsistency between [id] and [tripId]

---
+++ src/app/trips/[tripId]/diary/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import { useParams } from 'next/navigation';
+import { Toaster } from 'sonner';
+import TripDiary from '@/components/diary/TripDiary';
+
+export default function TripDiaryPage() {
+  const params = useParams();
+  const tripId = params?.tripId as string;
+  
+  return (
+    <>
+      <Toaster position="top-right" richColors />
+      <TripDiary tripId={tripId} />
+    </>
+  );
+}
